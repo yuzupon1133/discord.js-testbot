943d3c740a5b3165e0888203f6317df2375a6b0f
yuzupon1133 <65931718+yuzupon1133@users.noreply.github.com>
2022-01-12 17:30:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const { Client, Intents } = require('discord.js')
 const client = new Client({ intents: Object.keys(Intents.FLAGS) })
-var ary = [], tmp = [], mem = [], message;
+var timername = [], timernow = [], timersecond = [], memo = [], message;
 client.on('ready', () => {
   console.log(`${client.user.tag} でログインしています。`)
 })
-var command = {'memo':{'add':{'%s':'@mas'},'delete':{'%s':'@mds'},'list':'@ml'},'ping':'@p','help':'@h'};
-var info = {'memo':'!memo (add \'string\'|delete \'string\'|list)','ping':'!ping','help':'!help'};
-var com = ['memo','ping','help'];
+var command = {'memo':{'add':{'%s':'@mas'},'delete':{'%s':'@mds'},'list':'@ml'},'ping':'@p','help':'@h','timer':{'set':{'%i':{'%u':'@tsi','%s':'@tsis'}},'reset':{'%s':'@trs'},'list':'@tl'}};
+// %s string, %i int, %u undefined
+var info = {'memo':'!memo (add \'内容\'|delete \'内容\'|list)','ping':'!ping','help':'!help','timer':'!timer (set \'秒数\' (\'題名\')|reset \'題名\'|list)'};
+var com = ['help','ping','memo','timer'];
+
+function getTimer () {
+  let tmp = '';
+  for (i = 0; i < timername.length; i++) {
+    tmp += `\'${timername[i]}\' 残り${Math.floor(timernow[i]/1000)+timersecond[i]-Math.floor(Date.now()/1000)}秒\n`;
+  }
+  return tmp;
+}
+
+function sizensu (int) {
+  return (Math.ceil(int) == int && int >= 0);
+}
 
 function getHelp () {
   let tmp = '';
@@ -24,15 +37,18 @@ function discommand (content) {
       return;
     }
     for (i = 0; i < tmp.length; i++) {
-      if (tmp2[tmp[i] == undefined]) {
+      if (tmp2['%s'] != undefined && typeof(tmp[i]) == 'string') {
+        tmp2 = tmp2['%s'];
+      } else if (tmp2['%i'] != undefined && !isNaN(Number(tmp[i]))) {
+        tmp2 = tmp2['%i'];
+      } else if (tmp2[tmp[i]] == undefined) {
         return info[tmp[0]];
       } else {
-        if (tmp2['%s'] != undefined && typeof(tmp[i]) == 'string') {
-          tmp2 = tmp2['%s'];
-        } else {
-          tmp2 = tmp2[tmp[i]];
-        }
+        tmp2 = tmp2[tmp[i]];
       }
+    }
+    if (tmp2['%u'] != undefined) {
+      return tmp2['%u'];
     }
     if (typeof(tmp2) == 'object' || tmp2 == undefined) {
       return info[tmp[0]];
@@ -55,44 +71,86 @@ client.on('messageCreate', async msg => { if (!msg.author.bot) {
     if (String(tmp).charAt(0) == '!') {
       msg.channel.send(`${cnts[0].substr(1)}コマンドはこのように指定してください。\`\`\`javascript\n${tmp}\`\`\``)
     } else {
+      console.log(`コマンド実行: ${tmp}`);
+      ttmp = Date.now();
       switch(tmp) {
-      case '@p':
-        msg.channel.send(`pong! on ${client.ws.ping}ms`);
-        break;
-      case '@mas':
-        mem.push(cnts[2]);
-        msg.channel.send(`${cnts[2]}をメモに追加しました。`);
-        break;
-      case '@mds':
-        if (mem.indexOf(cnts[2]) == -1) {
-          msg.channel.send(`${cnts[2]}というメモはありません。`);
-        } else {
-          mem.splice(mem.indexOf(cnts[2]), 1);
-        msg.channel.send(`${cnts[2]}をメモから削除しました。`);
-        }
-        break;
-      case '@ml':
-        msg.channel.send(`メモ一覧です。\`\`\`${mem.join('\n')}\`\`\``);
-        break;
-      case '@h':
-        msg.channel.send(`コマンド一覧です。\`\`\`javascript\n${getHelp()}\`\`\``);
+        case '@p':
+          msg.channel.send(`pong! on ${client.ws.ping}ms`);
+          break;
+        case '@mas':
+          memo.push(cnts[2]);
+          msg.channel.send(`${cnts[2]}をメモに追加しました。`);
+          break;
+        case '@mds':
+          if (memo.indexOf(cnts[2]) == -1) {
+            msg.channel.send(`${cnts[2]}というメモはありません。`);
+          } else {
+            memo.splice(memo.indexOf(cnts[2]), 1);
+          msg.channel.send(`${cnts[2]}をメモから削除しました。`);
+          }
+          break;
+        case '@ml':
+          msg.channel.send(`メモ一覧です。\`\`\`${memo.join('\n')}\`\`\``);
+          break;
+        case '@h':
+          msg.channel.send(`コマンド一覧です。\`\`\`javascript\n${getHelp()}\`\`\``);
+          break;
+        case '@tsi':
+          if (sizensu(cnts[2])) {
+            timername.push(ttmp);
+            timernow.push(ttmp);
+            timersecond.push(Number(cnts[2]));
+            msg.channel.send(`${ttmp} のタイマーを${cnts[2]}秒に設定しました。`);
+          } else {
+            msg.channel.send(`秒数は自然数を指定してください。`);
+          }
+          break;
+        case '@tsis':
+          if (sizensu(cnts[2])) {
+            if (timername.indexOf(cnts[3]) != -1) {
+              timername.push(ttmp);
+              timernow.push(ttmp);
+              timersecond.push(Number(cnts[2]));
+              msg.channel.send(`${cnts[3]} のタイマーは既に使われています。\n代わりに、${ttmp} のタイマーを${cnts[2]}秒に設定しました。`);
+            } else {
+              timername.push(cnts[3]);
+              timernow.push(ttmp);
+              timersecond.push(Number(cnts[2]));
+              msg.channel.send(`${cnts[3]} のタイマーを${cnts[2]}秒に設定しました。`);
+            }
+          } else {
+            msg.channel.send(`秒数は自然数を指定してください。`);
+          }
+          break;
+        case '@trs':
+          if (timername.indexOf(cnts[2]) != -1) {
+            timernow.splice(timername[timername.indexOf(cnts[2])], 1);
+            timersecond.splice(timername[timername.indexOf(cnts[2])], 1);
+            timername.splice(timername[timername.indexOf(cnts[2])], 1);
+            msg.channel.send(`${cnts[2]} のタイマーをリセットしました。`);
+          } else {
+            msg.channel.send(`${cnts[2]} というタイマーはありません。`);
+          }
+          break;
+        case '@tl':
+          msg.channel.send(`タイマー一覧です。\`\`\`javascript\n${getTimer()}\`\`\``);
       }
     }
   }
 }})
 
-// const log = function(){
-//   for (i = 0; i < tmp.length; i++) {
-//     tmp[i] -= 1;
-//     if (tmp[i] <= 0) {
-//       message.channel.send(`${ary[i]}秒のタイマーが終了しました。`);
-//       tmp.splice(i, 1);
-//       ary.splice(i, 1);
-//       i--;
-//     }
-//   }
-// };
+const log = function(){
+  for (i = 0; i < timername.length; i++) {
+    if (Math.floor(timernow[i]/1000)+timersecond[i]-Math.floor(Date.now()/1000) <= 0) {
+      message.channel.send(`${timername[i]} のタイマーが終了しました。`);
+      timernow.splice(i, 1);
+      timersecond.splice(i, 1);
+      timername.splice(i, 1);
+      i--
+    }
+  }
+};
 
-// setInterval(log, 100);
+setInterval(log, 500);
 
 client.login(null)
